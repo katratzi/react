722169155bc129a269ac8e143df0d97486d21357
LIANLIQ01\Richard <ric_clifford@hotmail.com>
2019-07-24 21:41:56 +0200

    react - handlign click event

--- try-reactjs/src/App.js
+++ try-reactjs/src/App.js
@@ -1,16 +1,30 @@
 import React from "react";
+import ReactComp, { Component } from "react";
 import PostList from "./posts/PostList";
 
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <PostList />
-      </header>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <PostList />
+//       </header>
+//     </div>
+//   );
+// }
+
+// ES6 coding
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <PostList />
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
--- try-reactjs/src/posts/PostDetail.js
+++ try-reactjs/src/posts/PostDetail.js
@@ -1,12 +1,24 @@
 import React, { Component } from "react";
 
 class PostDetail extends Component {
+  constructor(props) {
+    // need this to get the properties for use in the titleWasClicked
+    super(props);
+    this.titleWasClicked = this.titleWasClicked.bind(this);
+  }
+
+  titleWasClicked(event) {
+    event.preventDefault();
+    alert(this.props);
+    console.log(this.props);
+  }
+
   render() {
     const { post } = this.props;
     //   const post = this.props.post;
     return (
       <div>
-        <h1>{post.title}</h1>
+        <h1 onClick={this.titleWasClicked}>{post.title}</h1>
         <p>{post.content}</p>
       </div>
     );
